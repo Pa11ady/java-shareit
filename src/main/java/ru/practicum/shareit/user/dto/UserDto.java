000286fff2d4895bfb0f282503ece0526dc87fe8
Pavel <pa11ady@yandex.ru>
2022-08-12 19:09:50 +0300

    UserDto

--- src/main/java/ru/practicum/shareit/user/dto/UserDto.java
+++ src/main/java/ru/practicum/shareit/user/dto/UserDto.java
@@ -1,10 +1,11 @@
 package ru.practicum.shareit.user.dto;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserDto {
     private Long id;
     private String name;        //имя или логин пользователя;
