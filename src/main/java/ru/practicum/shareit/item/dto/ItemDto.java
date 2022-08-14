c58cd1dba772d2893587d637be545b839e6706b6
Pavel <pa11ady@yandex.ru>
2022-08-14 23:42:43 +0300

    ItemMapper

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,17 +1,28 @@
 package ru.practicum.shareit.item.dto;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 import ru.practicum.shareit.requests.ItemRequest;
 import ru.practicum.shareit.user.User;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class ItemDto {
     private Long id;
     private String name;            //краткое название
     private String description;     //развёрнутое описание
+
+    @Getter(AccessLevel.NONE) @Setter(AccessLevel.NONE)
     private boolean available;      //доступна или нет вещь для аренды;
-    private User owner;             //владелец вещи;
-    private ItemRequest request;    //если создано по запросу, то ссылка на запрос
+    private Long owner;             //владелец вещи;
+    private Long request;           //если создано по запросу, то ссылка на запрос
+
+    public boolean getAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
 }
--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -2,13 +2,13 @@ package ru.practicum.shareit.item.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import ru.practicum.shareit.common.AbstractEntity;
 import ru.practicum.shareit.requests.ItemRequest;
 import ru.practicum.shareit.user.User;
 
 @Getter
 @Setter
-public class Item {
-    private Long id;
+public class Item extends AbstractEntity {
     private String name;            //краткое название
     private String description;     //развёрнутое описание
     private boolean available;      //доступна или нет вещь для аренды;
