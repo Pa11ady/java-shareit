98186db70b989521762a863735fed556e711d3de
Pavel <pa11ady@yandex.ru>
2022-08-28 02:19:31 +0300

    fix param

--- src/main/java/ru/practicum/shareit/item/ItemRepository.java
+++ src/main/java/ru/practicum/shareit/item/ItemRepository.java
@@ -2,7 +2,6 @@ package ru.practicum.shareit.item;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import ru.practicum.shareit.item.model.Item;
 
 import java.util.List;
@@ -14,5 +13,5 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
             "(lower(i.name) like lower(concat('%', :text,'%')) or " +
             "lower(i.description) like lower(concat('%', :text,'%'))) " +
             "and i.available = true")
-    List<Item> search(@Param("text") String text);
+    List<Item> search(String text);
 }
